a235c11aef95c5d3eebe3e22462c80bcbad79d20
critoma <cristianvtoma@gmail.com>
2023-02-09 11:46:02 +0200

    Update p14_arr.s

--- labs/workspacearmassembly/arm32/p05_branches_condctrlflow_thumb/p14_arr.s
+++ labs/workspacearmassembly/arm32/p05_branches_condctrlflow_thumb/p14_arr.s
@@ -1,11 +1,16 @@
-/* for (int i = 0; i < 100; i++) a[i] =  i; / see why may not work */
+
+
+/* for (int i = 0; i < 100; i++) a[i] =  i;  */
 @ use as for assembling and gcc for link-editing
 
-.text
+.data
 
+ a: .word 0x00000000
+
+.text
 .global main
 main:
- ldr r1, =addr_of_a
+ ldr r1, addr_of_a
  mov r2, #0
 loop:
  cmp r2, #100
@@ -17,4 +22,4 @@ b loop
 end:
  bx lr
 
-addr_of_a: .word 0x00000000
+addr_of_a: .word a
